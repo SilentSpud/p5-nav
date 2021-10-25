0980b04f84397b150f70a05fbe0bc9f298466b18
Silent Spud <silent@silentdev.io>
2021-10-25 00:49:41 -0600

    start confidants

---
+++ data/confidants.ts
@@ -0,0 +1,88 @@
+interface Confidant {
+  link: string;
+  card: string;
+}
+
+interface ConfidantMap {
+  [index: string]: Confidant;
+}
+
+const confidantList: ConfidantMap = {
+  chariot: {
+    link: "https://hardcoregamer.com/2017/04/04/persona-5-confidant-guide-chariot-ryuji-sakamoto/",
+    card: "../assets/chariot.jpg"
+  },
+  death: {
+    link: "https://hardcoregamer.com/2017/04/04/persona-5-confidant-guide-death-tae-takemi/",
+    card: "../assets/death.jpg"
+  },
+  devil: {
+    link: "https://hardcoregamer.com/2017/04/04/persona-5-confidant-guide-devil-ichiko-ohya",
+    card: "../assets/devil.jpg"
+  },
+  emperor: {
+    link: "https://hardcoregamer.com/2017/04/04/persona-5-confidant-guide-emperor-yusuke-kitagawa/",
+    card: "../assets/emperor.jpg"
+  },
+  empress: {
+    link: "https://hardcoregamer.com/2017/04/04/persona-5-confidant-guide-empress-haru-okumura/",
+    card: "../assets/empress.jpg"
+  },
+  fortune: {
+    link: "https://hardcoregamer.com/2017/04/04/persona-5-confidant-guide-fortune-chihaya-mifune/252884/",
+    card: "../assets/fortune.jpg"
+  },
+  hanged: {
+    link: "https://hardcoregamer.com/2017/04/04/persona-5-confidant-guide-hanged-munehisa-iwai/",
+    card: "../assets/hanged.jpg"
+  },
+  hermit: {
+    link: "https://hardcoregamer.com/2017/04/04/persona-5-confidant-guide-hermit-futaba-sakura",
+    card: "../assets/hermit.jpg"
+  },
+  hierophant: {
+    link: "https://hardcoregamer.com/2017/04/04/persona-5-confidant-guide-hierophant-sojiro-sakura",
+    card: "../assets/hierophant.jpg"
+  },
+  lovers: {
+    link: "https://hardcoregamer.com/2017/04/04/persona-5-confidant-guide-lovers-ann-takamaki/",
+    card: "../assets/lovers.jpg"
+  },
+  moon: {
+    link: "https://hardcoregamer.com/2017/04/04/persona-5-confidant-guide-moon-yuuki-mishima/",
+    card: "../assets/moon.jpg"
+  },
+  priestess: {
+    link: "https://hardcoregamer.com/2017/04/04/persona-5-confidant-guide-priestess-makoto-niijima/",
+    card: "../assets/priestess.jpg"
+  },
+  star: {
+    link: "https://hardcoregamer.com/2017/04/04/persona-5-confidant-guide-star-hifumi-togo/",
+    card: "../assets/star.jpg"
+  },
+  sun: {
+    link: "https://hardcoregamer.com/2017/04/04/persona-5-confidant-guide-sun-toranosuke-yoshida/",
+    card: "../assets/sun.jpg"
+  },
+  temperance: {
+    link: "https://hardcoregamer.com/2017/04/04/persona-5-confidant-guide-temperance-sadayo-kawakami/",
+    card: "../assets/temperance.jpg"
+  },
+  tower: {
+    link: "https://hardcoregamer.com/2017/04/04/persona-5-confidant-guide-tower-shinya-oda/",
+    card: "../assets/tower.jpg"
+  },
+  faith: {
+    link: "https://hardcoregamer.com/2020/03/31/persona-5-royal-confidant-guide-faith-kasumi-yoshizawa/370469/",
+    card: "../assets/faith.jpg"
+  },
+  justice: {
+    link: "https://hardcoregamer.com/2020/03/31/persona-5-royal-confidant-guide-justice-goro-akechi/370472/",
+    card: "../assets/justice.jpg"
+  },
+  councillor: {
+    link: "https://hardcoregamer.com/2020/03/31/persona-5-royal-confidant-guide-councillor-takuto-maruki/370462/",
+    card: "../assets/councillor.jpg"
+  },
+};
+export default confidantList;
