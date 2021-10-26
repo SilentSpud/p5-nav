cbeb77186267efe6ed5194d36790c1271f3b5e12
Silent Spud <silent@silentdev.io>
2021-10-26 16:37:10 -0600

    answer initial

--- src/confidantGen.mjs
+++ src/confidantGen.mjs
@@ -1,12 +1,15 @@
 "use strict";
 // This file uses a "revolving door"-esque system to generate confidant data from various source html files using a mock DOM
 // With this, you can build code for one entry and test it against the entire database before writing
-import { JSDOM } from "jsdom";
+// Bear in mind, the html files were edited heavily beforehand to be easier to work with
+import {
+  JSDOM
+} from "jsdom";
 import fs from "fs";
 const confidants = JSON.parse(fs.readFileSync(`data/confidant.json`));
 // Edit as needed
-const breakAtOne = false;
-const writeDataToFile = false;
+const breakAtOne = true;
+const writeDataToFile = true;
 
 // Useful functions
 const parsechild = (elem) => ((elem.children.length > 0) ? elem.children[0].innerHTML : elem.innerHTML).trim();
@@ -14,33 +17,17 @@ const parseNum = (str) => isNaN(parseFloat(str)) ? str : parseFloat(str);
 const testNum = (str) => !isNaN(parseFloat(str));
 const parseBr = (elem) => (elem.children.length > 0 && elem.children[0].tagName == "BR") ? elem.innerHTML.trim().replace(/<br>\n/g, "") : elem.innerHTML.trim();
 const testRank = (rankNum) => (/^([1-9](\.5)?|MAX(\(ROMANCE\))?)$/g).test(rankNum)
-const parseRank = (rankVal) => {
-  if (testNum(rankVal)) {
-    return parseNum(rankVal);
-  }
-  if (rankVal === "MAX") {
-    return "Max";
-  }
-  if (rankVal === "MAX(ROMANCE)") {
-    return "Max (Romance)";
-  }
-  return false;
+const attrs = {
+  rankNum: "data-rank",
+  romance: "data-romance",
+  question: "data-question",
+  points: "data-points",
+  bad: "data-bad",
+  end: "data-end"
 }
-const runTests = (name, rawRank, inPair) => {
-  if (inPair)  {
-    console.log(`Confidant: ${name}, ${rawRank} | Pairing: ${!inPair ? 'Passed' : 'Failed' }`);
-    return false;
-  }
-  if (!testRank(rawRank)) {
-    console.log(`Confidant: ${name}, ${rawRank} | Basic: ${testRank(rawRank) ? 'Passed' : 'Failed' }`);
-    return false;
-  }
-  let rank = parseRank(rawRank)
-  if (!rank) {
-    console.log(`Confidant: ${name}, ${rawRank} | Complex: ${!!rank ? 'Passed' : 'Failed' }`);
-    return false;
-  }
-  return rank;
+
+const parseRow = (row) => {
+  const x = 5;
 }
 
 let confidantList = {};
@@ -52,42 +39,41 @@ for (let name in confidants) {
   const document = window.document;
   // Setup end
 
-  let elems = document.querySelectorAll("main > article > div > table, main > article > div > h2 > strong, main > article > div > p > em");
-  let ranks = {};
-  let curRank;
-  let inPair = false;
-  for (let el of elems) {
-    if (el.tagName === "STRONG") {
-      let RankText = parseBr(el);
-      if (RankText == "Benefits:") continue;
-      let rank = runTests(name, RankText.slice(5), inPair);
-      if (!rank) continue;
-      inPair = true;
-      curRank = rank;
-    } else if (el.tagName === "TABLE") {
-      // Skip benefits tables
-      if (el.children.length > 0 && el.children[0].children.length > 0 && el.children[0].children[0].children.length > 0 && el.children[0].children[0].children[0].innerHTML == "<strong>Rank</strong>") continue;
-      if (!inPair)  {
-        console.log(`Confidant: ${name}, Table | Pairing: ${inPair ? 'Passed' : 'Failed' } | First Cell: "${el.children[0].children[0].children[0].innerHTML}"`);
-        continue;
+  let rankList = {}
+  let rankElems = document.body.children;
+  for (let rankEl of rankElems) {
+    let rankNum = parseNum(rankEl.getAttribute(attrs.rankNum));
+    let rank = {
+      questions: {}
+    };
+    if (rankEl.getAttribute(attrs.romance)) rank.romance = true;
+
+    for (let rankComp of rankEl.children) {
+      if (rankComp.tagName == "TABLE") {
+        let rows = rankComp.children[0].children;
+        for (let row of rows) {
+          let rowAnswers = [];
+          for (let cell of row.children) {
+            let answer = {
+              text: cell.innerHTML
+            }
+            if (cell.getAttribute(attrs.points)) answer.points = parseNum(cell.getAttribute(attrs.points));
+            if (cell.getAttribute(attrs.romance)) answer.romance = true;
+            if (cell.getAttribute(attrs.bad)) answer.bad = true;
+            if (cell.getAttribute(attrs.end)) answer.end = true;
+            rowAnswers.push(rowAnswers);
+          }
+
+          rank.questions[row.getAttribute(attrs.question)] = rowAnswers;
+        }
       }
-      inPair = false;
-      //console.log(`Table`);
-    } else if (el.tagName === "EM") {
-      if (!inPair)  {
-        console.log(`Confidant: ${name}, Paragraph | Pairing: ${inPair ? 'Passed' : 'Failed' }`);
-        continue;
-      }
-      //console.log(`Table`);
-    } else {
-      console.log(`Confidant: ${name}, Unknown | Tag: ${el.tagName}`);
-      continue;
     }
+
+    console.log(rank);
+    rankList[rankNum] = rank;
   }
-  if (inPair)  {
-    console.log(`Confidant: ${name}, end | Pairing: ${!inPair ? 'Passed' : 'Failed' }`);
-    continue;
-  }
+
+
 
   // modify structure as needed
   let newConfidant = {
