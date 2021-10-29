333694373cc597ebdd0180a86843439430ba655b
Silent Spud <silent@silentdev.io>
2021-10-29 12:59:48 -0600

    more refactoring

--- src/tableMaker.tsx
+++ src/tableMaker.tsx
@@ -1,49 +1,41 @@
-/* eslint-disable react/jsx-key */
 import React from "react";
 import { HeaderGroup, Row, useFlexLayout, useGlobalFilter, useSortBy, useTable } from "react-table";
 import { FontAwesomeIcon } from '@fortawesome/react-fontawesome';
 import { faSort, faSortDown, faSortUp } from '@fortawesome/free-solid-svg-icons';
 const Icon = FontAwesomeIcon;
 
-const parseRow = (row: Row, parseFunction) => {
+const parseRow = (row: Row, parseFunction, i: number) => {
   return (
-    <div {...row.getRowProps()}>
+    <div key={i} {...row.getRowProps()}>
       {row.cells.map((cell) => parseFunction(cell))}
     </div>
   );
 }
 
-const renderHeader = (headers: HeaderGroup[], setGlobalFilter: (filterValue: any) => void) => {
-  return headers.map((row) => (
-    <div {...row.getHeaderGroupProps()}>
+const renderHeader = (headers: HeaderGroup[], setGlobalFilter: (filterValue) => void) => {
+  return headers.map((row, i) => (
+    <div key={i} {...row.getHeaderGroupProps()}>
       {row.headers.map((cell) => {
         if (cell.id == "title_0") {
-          return (<div {...cell.getHeaderProps({ className: "searchbox" })}>{globalFilter(setGlobalFilter)}</div>);
+          return <div {...cell.getHeaderProps({ className: "searchbox" })}>{searchForm(setGlobalFilter)}</div>;
         } else {
-          return (<div {...cell.getHeaderProps(cell.getSortByToggleProps())}>{cell.render("Header")}<span>{
-            cell.canSort && (cell.isSorted ? cell.isSortedDesc ? <Icon icon={faSortUp} /> : <Icon icon={faSortDown} /> : <Icon icon={faSort} />)
-          }</span></div>);
+          return <div {...cell.getHeaderProps(cell.getSortByToggleProps())}>
+            {cell.render("Header")}
+            {cell.canSort && <span>{cell.isSortedDesc && <Icon icon={faSortUp} /> || cell.isSorted && <Icon icon={faSortDown} /> || <Icon icon={faSort} />}</span>}
+          </div>;
         }
       })}
-    </div>))
+    </div>
+  ));
 }
 
-const globalFilter = (setGlobalFilter: (filterValue: any) => void) => {
-  const form = (<input type="search" placeholder="Search" className="text-light w-100 h-100 search" onChange={(key) => {
-    if (key.currentTarget.value.length == 0) {
-      setGlobalFilter(undefined);
-    }
-    else {
-      setGlobalFilter(key.currentTarget.value || undefined);
-    }
-  }}></input>);
-
-  return form;
-}
+const searchForm = (setGlobalFilter: (filterValue) => void) => (
+  <input type="search" placeholder="Search" className="text-light w-100 h-100 search" onChange={(key) => setGlobalFilter(key.currentTarget.value || undefined)}></input>
+);
 
 
 const prepareTable = (columnData, tableData, rowParser) => {
-  const sortId = columnData[0].columns ? columnData[0].columns[0].accessor : columnData[0].accessor ?? "";
+  const sortId = columnData[0]?.columns[0]?.accessor ?? columnData[0]?.accessor ?? "";
   const {
     getTableProps,
     getTableBodyProps,
@@ -56,18 +48,16 @@ const prepareTable = (columnData, tableData, rowParser) => {
     data: tableData,
     initialState: { sortBy: [{ id: sortId }] }
   }, useGlobalFilter, useSortBy, useFlexLayout);
-  return (
-    <section {...getTableProps({ className: "table table-dark" })}>
-      <header role="rowgroup">
-        {renderHeader(headerGroups, setGlobalFilter)}
-      </header>
-      <main {...getTableBodyProps()}>
-        {rows.map((row, i) => {
-          prepareRow(row);
-          return parseRow(row, rowParser);
-        })}
-      </main>
-    </section>
-  )
+  return <section {...getTableProps({ className: "table table-dark" })}>
+    <header role="rowgroup">
+      {renderHeader(headerGroups, setGlobalFilter)}
+    </header>
+    <main {...getTableBodyProps()}>
+      {rows.map((row, i) => {
+        prepareRow(row);
+        return parseRow(row, rowParser, i);
+      })}
+    </main>
+  </section>
 };
 export default prepareTable;
