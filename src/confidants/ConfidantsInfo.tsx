e5520b5df41eb9962b3aba691bc75307514a25bb
Silent Spud <silent@silentdev.io>
2021-10-31 11:36:17 -0600

    remove old confidants test

--- src/confidants/ConfidantsInfo.tsx
+++
@@ -1,8 +0,0 @@
-import React from "react";
-import Confidants from "../data/Confidants";
-
-
-const ConfidantsInfo = (): JSX.Element => {
-  return (<div>Hi</div>);
-}
-export default ConfidantsInfo;
