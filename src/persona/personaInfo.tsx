6798fd4f6e89a6b35270a0c8eb5b97aa15d78fed
Silent Spud <silent@silentdev.io>
2021-11-02 06:04:41 -0600

    slight update to info test

--- src/persona/personaInfo.tsx
+++ src/persona/personaInfo.tsx
@@ -1,66 +1,18 @@
 import React from "react";
+import { Col, Container, Row } from "react-bootstrap";
 import { useParams } from "react-router-dom";
 import { getPersona } from "../data";
 
-/* interface PersonaData {
-  name: string;
-  arcana: string;
-  level: number;
-  stats: {
-    strength: number;
-    magic: number;
-    endurance: number;
-    agility: number;
-    luck: number;
-  };
-  elems: {
-    physical: WeaknessLevels;
-    gun: WeaknessLevels;
-    fire: WeaknessLevels;
-    ice: WeaknessLevels;
-    electric: WeaknessLevels;
-    wind: WeaknessLevels;
-    psychic: WeaknessLevels;
-    nuclear: WeaknessLevels;
-    bless: WeaknessLevels;
-    curse: WeaknessLevels;
-  };
-  skills: {
-    [index: string]: number;
-  };
-  personality?: string;
-  shadow?: string;
-  special?: boolean;
-  max?: boolean;
-  dlc?: boolean;
-  note?: string;
-  rare?: boolean;
-  inherits?: string;
-  item?: string;
-  alarm?: string;
-  trait?: string;
-}*/
-
 export const personaInfo = (props) => {
   const { id } = useParams<{ id: string }>();
-  const personaId = decodeURIComponent(id);
-  const Persona = getPersona(personaId);
+  const Persona = getPersona(decodeURIComponent(id));
   return (
-    <table>
-      <tbody>
-        <tr>
-          <td>Name:</td>
-          <td>{Persona.name}</td>
-        </tr>
-        <tr>
-          <td>Arcana:</td>
-          <td>{Persona.arcana}</td>
-        </tr>
-        <tr>
-          <td>Level:</td>
-          <td>{Persona.level}</td>
-        </tr>
-      </tbody>
-    </table>
+    <Container {...props}>
+      <Row>
+        <Col>{Persona.name}</Col>
+        <Col>{Persona.arcana}</Col>
+        <Col>{Persona.level}</Col>
+      </Row>
+    </Container>
   );
 };
