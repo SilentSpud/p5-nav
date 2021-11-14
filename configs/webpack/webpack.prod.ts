ff36a7cf3c09cd53fe3e5464e1e0482567f6822e
Silent Spud <silent@silentdev.io>
2021-11-13 23:22:49 -0700

    remove webpack config

--- configs/webpack/webpack.prod.ts
+++
@@ -1,9 +0,0 @@
-import webpack from "webpack";
-import { merge } from "webpack-merge";
-import baseConfig from "./webpack.common";
-
-const config: webpack.Configuration = merge(baseConfig, {
-  mode: "production",
-});
-
-export default config;
