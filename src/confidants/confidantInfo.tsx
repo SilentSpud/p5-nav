a55fb5fc8778ec4e676772e6f5591181da1ef8de
Silent Spud <silent@silentdev.io>
2021-11-18 12:25:39 -0700

    benefits table

--- src/confidants/confidantInfo.tsx
+++ src/confidants/confidantInfo.tsx
@@ -1,7 +1,9 @@
 import React from "react";
-import { Col, Container, Row } from "react-bootstrap";
+import { Table } from "react-bootstrap";
 import { useRouter } from "next/router";
-import { getConfidant } from "../data";
+import { ConfidantBenefit, getConfidant } from "../data";
+
+const camel = (text: string) => text.charAt(0).toUpperCase() + text.slice(1);
 
 const useConfidant = () => {
   const router = useRouter();
@@ -17,9 +19,42 @@ const useConfidant = () => {
   return getConfidant(confidant);
 };
 
+
+const formatBenefits = (benefits: { [name: string]: ConfidantBenefit }) => {
+  const benefitsList: ConfidantBenefit[] = [];
+  for (let perkName in benefits) {
+    let perk = Object.assign({ name: perkName }, benefits[perkName]);
+    benefitsList.push(perk);
+  }
+  return benefitsList;
+};
+
 export const ConfidantInfo = () => {
   const confidant = useConfidant();
   if (!confidant) return null;
   console.log(confidant);
-  return <p>{confidant.name}</p>;
+  return (
+    <div className="confidant-info">
+      <h2>{camel(confidant.name)}</h2>
+      <h3>{confidant.character}</h3>
+      <Table striped bordered hover>
+        <thead>
+          <tr>
+            <th>Rank</th>
+            <th>Name</th>
+            <th>Description</th>
+          </tr>
+          </thead>
+        <tbody>
+          {confidant.benefits.map((row, index) => (
+            <tr key={index}>
+              <td>{row.rank}</td>
+              <td>{row.name}</td>
+              <td>{row.description}</td>
+            </tr>
+          ))}
+        </tbody>
+      </Table>
+    </div>
+  );
 };
