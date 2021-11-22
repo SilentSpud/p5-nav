1158477365686ab3182a31d90a4118af0f9f9033
Silent Spud <1478389+SilentSpud@users.noreply.github.com>
2021-11-22 15:25:39 -0700

    update next settings

--- next.config.js
+++ next.config.js
@@ -1,9 +1,9 @@
 /** @type {import('next').NextConfig} */
 const withYaml = require("next-plugin-yaml");
 module.exports = withYaml({
-  reactStrictMode: true/*,
+  reactStrictMode: true /*,
   images: {
     loader: "imgix",
     path: process.env.IMGIX_DOMAIN
-  },*/
+  },*/,
 });
