0ea848e0bebbe5fb2ecaec784b9108af09766fde
Silent Spud <1478389+SilentSpud@users.noreply.github.com>
2021-11-23 08:33:28 -0700

    add skills list

---
+++ src/skillList/SkillList.tsx
@@ -0,0 +1,92 @@
+import React from "react";
+import { useRouter } from "next/router";
+import { Row } from "react-table";
+import PrepareTable from "../tableMaker";
+import { Skills } from "../data";
+
+/*export interface SkillData {
+  name: string;
+  cost?: number;
+  effect: string;
+  element: string | ResElems | SkillElems;
+  talk?: string;
+  fuse?: string | string[];
+  card?: string;
+  unique?: string;
+  dlc?: boolean;
+  note?: string;
+}*/
+const Headers = [
+  {
+    Header: "Skill List",
+    id: "title",
+    columns: [
+      { Header: "Name", accessor: "name" },
+      { Header: "Cost", accessor: "cost" },
+      { Header: "Effect", accessor: "effect" },
+      { Header: "PersonElementality", accessor: "element" },
+    ],
+  },
+  {
+    Header: "Flags",
+    id: "flags",
+    columns: [
+      { Header: "Talk", accessor: "talk" },
+      { Header: "Fuse", accessor: "fuse" },
+      { Header: "Card", accessor: "card" },
+      { Header: "Unique", accessor: "unique" },
+      { Header: "DLC", accessor: "dlc" },
+      { Header: "Note", accessor: "note" },
+    ],
+  }
+];
+
+interface CellData {
+  name: string | JSX.Element;
+  cost: number | JSX.Element;
+  effect: string | JSX.Element;
+  element: string | JSX.Element;
+  talk: string | JSX.Element;
+  fuse: string | JSX.Element;
+  card: string | JSX.Element;
+  unique: string | JSX.Element;
+  dlc: string | JSX.Element;
+  note: string | JSX.Element;
+}
+
+const parseSkills = () => {
+  const pList: CellData[] = [];
+  for (const skill of Skills) {
+    pList.push({
+      name: skill.name,
+      cost: skill.cost ?? <>&nbsp;</>,
+      effect: skill.effect,
+      element: skill.element.toString() ?? <>&nbsp;</>,
+      talk: skill.talk ?? <>&nbsp;</>,
+      fuse: (skill.fuse && (typeof skill.fuse == "string" ? skill.fuse : skill.fuse.join(", "))) ?? <>&nbsp;</>,
+      card: skill.card ?? <>&nbsp;</>,
+      unique: skill.unique ?? <>&nbsp;</>,
+      dlc: skill.dlc ? <>true</> : <>&nbsp;</>,
+      note: skill.note ?? <>&nbsp;</>,
+    });
+  }
+  return pList;
+};
+
+const Row = ({ rowProps, cells }): JSX.Element => (
+  <div {...rowProps()}>
+    {cells.map((cell, index): JSX.Element => (
+      <div {...cell.getCellProps()} key={index}>{cell.render("Cell")}</div>
+    ))}
+  </div>
+)
+
+
+export const SkillList = (): JSX.Element => {
+  const data = parseSkills();
+  const rowParser: (row: Row<{}>, index: number) => JSX.Element = (row, index) => (
+    <Row rowProps={row.getRowProps} cells={row.cells} />
+  )
+  return PrepareTable({ columns: Headers, data, rowParser, className: "skills" });
+};
+export default SkillList;
