a33e7618e5692aa686a97acd3ac79c8a666a3287
Silent Spud <1478389+SilentSpud@users.noreply.github.com>
2021-11-22 19:35:46 -0700

    remove imgix as code is running on netlify

--- next.config.js
+++ next.config.js
@@ -2,9 +2,5 @@
 const withYaml = require("next-plugin-yaml");
 module.exports = withYaml({
   reactStrictMode: true,
-  generateBuildId: () => "build" /*
-  images: {
-    loader: "imgix",
-    path: process.env.IMGIX_DOMAIN
-  },*/,
+  generateBuildId: () => "build",
 });
