73f068272a3fea25331d7b77f46f2b7f2a0eede7
Silent Spud <1478389+SilentSpud@users.noreply.github.com>
2021-11-25 08:25:15 -0700

    remove class from the table

--- src/table/index.tsx
+++ src/table/index.tsx
@@ -1,11 +1,4 @@
 export * from "./tableMaker";
 export * from "./header";
-
-import React from "react";
-import PrepareTable, { PrepareTableProps } from "./tableMaker";
-class FlexTable extends React.Component {
-  render(this: { props: Readonly<PrepareTableProps> }) {
-    return <PrepareTable {...this.props}></PrepareTable>;
-  }
-}
-export default FlexTable;
+import PrepareTable from "./tableMaker";
+export default PrepareTable;
