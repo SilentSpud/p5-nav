70ef3feee0b608ab2503fcd2d9105851c4ed29d9
Silent Spud <1478389+SilentSpud@users.noreply.github.com>
2021-11-27 16:25:48 -0700

    add status effect data

---
+++ src/data/Status.ts
@@ -0,0 +1,118 @@
+export type StatusInfo = {
+  name: string;
+  effect?: string[];
+  effects?: Effects;
+  technical?: Technical[];
+};
+export type Technical = {
+  name: string;
+  heart?: boolean; // Requires having read "Knowing the Heart"
+};
+
+export type Effects = {
+  blockAction?: true; // Unable to take action
+  blockNegotiate?: true; // Unable to negotiate with target
+  blockSkill?: true; // Unable to use any skills
+  cureOnHit?: true; // Ailment cured if the target is attacked
+  cureOnNegotiate?: true; // Ailment cured when the player negotiates with the target
+  fleeOnNegotiate?: true; // Will sometimes run from battle. Will run if negotiated with
+};
+
+export const Status: StatusInfo[] = [
+  {
+    name: "Down",
+    effect: ["Unable to evade", "Affected characters take 125% damage", "Downed characters recover on their turn"],
+  },
+  { name: "Burn", effect: ["Take 10% max HP damage after every action"], technical: [{ name: "Wind" }, { name: "Nuclear" }] },
+  {
+    name: "Freeze",
+    effects: {
+      blockAction: true,
+      blockNegotiate: true,
+      cureOnHit: true,
+    },
+    effect: ["Resistance to Physical and Gun is null"],
+    technical: [{ name: "Physical" }, { name: "Gun" }, { name: "Nuclear" }],
+  },
+  {
+    name: "Shock",
+    effects: {
+      blockAction: true,
+      blockNegotiate: true,
+    },
+    effect: ["If the target is hit with a melee attack, there is a slight chance the attacker will become shocked"],
+    technical: [{ name: "Physical" }, { name: "Gun" }, { name: "Nuclear" }],
+  },
+  {
+    name: "Dizzy",
+    effects: {
+      cureOnHit: true,
+      cureOnNegotiate: true,
+    },
+    effect: ["Accuracy greatly reduced"],
+    technical: [{ name: "any" }],
+  },
+  {
+    name: "Forget",
+    effects: {
+      blockSkill: true,
+      cureOnHit: true,
+      cureOnNegotiate: true,
+    },
+    technical: [{ name: "Psychic" }, { name: "Electric", heart: true }],
+  },
+  {
+    name: "Sleep",
+    effects: {
+      blockAction: true,
+      blockNegotiate: true,
+      cureOnHit: true,
+    },
+    effect: ["+10% max HP/SP per turn"],
+    technical: [{ name: "any" }],
+  },
+  {
+    name: "Confuse",
+    effects: {
+      cureOnHit: true,
+      cureOnNegotiate: true,
+    },
+    effect: ["Random chance to throw away money or items", "Allies can also use healing items", "Enemies can skip their turn"],
+    technical: [{ name: "Psychic" }, { name: "Wind", heart: true }, { name: "Gun", heart: true }],
+  },
+  {
+    name: "Fear",
+    effects: {
+      fleeOnNegotiate: true,
+    },
+    effect: ["High chance of ignoring commands"],
+    technical: [{ name: "Psychic" }, { name: "Ice", heart: true }],
+  },
+  {
+    name: "Despair",
+    effects: {
+      blockAction: true,
+      cureOnNegotiate: true,
+    },
+    effect: ["SP will decrease by 5%, and the character becomes incapacitated after 3 turns while in despair."],
+    technical: [{ name: "Psychic" }, { name: "Curse", heart: true }],
+  },
+  {
+    name: "Rage",
+    effects: {
+      blockAction: true,
+      cureOnNegotiate: true,
+    },
+    effect: ["+100% Attack", "-50% Defense, Evasion, Accuracy", "Ignores commands, automatically uses basic melee"],
+    technical: [{ name: "Psychic" }, { name: "Fire", heart: true }],
+  },
+  {
+    name: "Brainwash",
+    effect: ["Random chance to heal/buff an enemy or attack an ally", "Special dialogue options while negotiating"],
+    technical: [{ name: "Psychic" }, { name: "Bless", heart: true }],
+  },
+  {
+    name: "Hunger",
+    effect: ["Greatly lowers attack power", "Doubles the HP/SP cost of skills"],
+  },
+];
