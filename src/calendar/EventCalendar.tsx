5cb9620bd95e469adfbf316b8f055188f472b8eb
Silent Spud <1478389+SilentSpud@users.noreply.github.com>
2021-11-29 06:51:32 -0700

    refactor calendar

--- src/calendar/EventCalendar.tsx
+++ src/calendar/EventCalendar.tsx
@@ -1,13 +1,8 @@
-import React, { useMemo } from "react";
-import { Day, Month } from "./CalendarBody";
-import Navbar from "./Navbar";
-import { Calendar, parseYear, EventItem } from "./CalendarController";
+import { useMemo } from "react";
+import { parseYear, EventItem } from "./CalendarController";
 import { ClassroomQuestions } from "../data";
-import useStorage from "react-use-localstorage2";
 
-const gameStart = "2016-04-01T06:00:00.000Z";
-
-const LoadQuestions = () =>
+export const LoadQuestions = () =>
   useMemo(() => {
     const questions: EventItem[] = [];
     for (const day of ClassroomQuestions) {
@@ -21,17 +16,3 @@ const LoadQuestions = () =>
     }
     return questions;
   }, []);
-
-export const EventCalendar = () => {
-  const [month, setMonth] = useStorage("date", gameStart);
-  const monthHandler = (date: Date) => setMonth(JSON.stringify(date).replace(/"/g, ""));
-  const classQs = LoadQuestions();
-  return (
-    <Calendar month={new Date(month as string)} onMonthChange={monthHandler} events={classQs}>
-      <Navbar />
-      <Month>
-        <Day />
-      </Month>
-    </Calendar>
-  );
-};
---
+++ src/pages/calendar/index.tsx
@@ -0,0 +1,25 @@
+import Head from "next/head";
+import React from "react";
+import useStorage from "react-use-localstorage2";
+import { Calendar, Day, LoadQuestions, Month, Navbar } from "../../calendar";
+
+const EventCalendar = () => {
+  const [month, setMonth] = useStorage("date", "2016-04-01T06:00:00.000Z"),
+    monthHandler = (date: Date) => setMonth(JSON.stringify(date).replace(/"/g, "")),
+    classQs = LoadQuestions(),
+    monthDate = new Date(month as string);
+  return (
+    <>
+      <Head>
+        <title>Calendar - rNav</title>
+      </Head>
+      <Calendar month={monthDate} onMonthChange={monthHandler} events={classQs}>
+        <Navbar />
+        <Month>
+          <Day />
+        </Month>
+      </Calendar>
+    </>
+  );
+};
+export default EventCalendar;
