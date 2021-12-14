68c29f36590628bd6c54eaf6ab27e6d7133229b3
Silent Spud <1478389+SilentSpud@users.noreply.github.com>
2021-12-14 14:03:52 -0700

    update negotiation table rendering

--- src/negotiation/negotiation.tsx
+++ src/negotiation/negotiation.tsx
@@ -1,8 +1,8 @@
 import React from "react";
 import { Table } from "react-bootstrap";
-import { NegotiationList, NegotiationReaction as Reaction, NegotiationReactions as Reactions, Negotiations } from "../data";
+import { NegotiationList, NegotiationAnswer, NegotiationReaction as Reaction, NegotiationReactions as Reactions } from "../data";
 
-export const Answer = (props: { value: Reaction | Reactions }) => {
+const Answer = (props: { value: Reaction | Reactions }) => {
   let ansVal: Reactions,
     ansUnknown = false;
   switch (typeof props.value) {
@@ -28,40 +28,41 @@ export const Answer = (props: { value: Reaction | Reactions }) => {
   }
 };
 
-export const parseQuestions = (questionList: NegotiationList) => {
-  const tableList: JSX.Element[] = [];
-  let tNum = 0;
-  for (const question in questionList) {
-    const answers = questionList[question];
+const QuestionTable = ({ question, answers }: { question: string; answers: NegotiationAnswer[]; }) => {
 
-    tableList.push(
-      <Table key={tNum} bordered>
-        <thead>
-          <tr>
-            <td colSpan={5}>{question}</td>
+  return (
+    <Table bordered variant="dark">
+      <thead>
+        <tr>
+          <th colSpan={5} className="question">{question}</th>
+        </tr>
+        <tr>
+          <th></th>
+          <th>Gloomy</th>
+          <th>Irritable</th>
+          <th>Timid</th>
+          <th>Upbeat</th>
+        </tr>
+      </thead>
+      <tbody>
+        {answers.map((answer, index) => (
+          <tr key={index}>
+            <td>{answer.text}</td>
+            <Answer value={answer.gloomy} />
+            <Answer value={answer.irritable} />
+            <Answer value={answer.timid} />
+            <Answer value={answer.upbeat} />
           </tr>
-          <tr>
-            <th></th>
-            <th>Gloomy</th>
-            <th>Irritable</th>
-            <th>Timid</th>
-            <th>Upbeat</th>
-          </tr>
-        </thead>
-        <tbody>
-          {answers.map((ans, i) => (
-            <tr key={i}>
-              <td>{ans.text}</td>
-              <Answer value={ans.gloomy} />
-              <Answer value={ans.irritable} />
-              <Answer value={ans.timid} />
-              <Answer value={ans.upbeat} />
-            </tr>
-          ))}
-        </tbody>
-      </Table>
-    );
-    tNum++;
+        ))}
+      </tbody>
+    </Table>
+  )
+}
+
+export const QuestionList = ({ questions }: { questions: NegotiationList }) => {
+  const tableList: JSX.Element[] = [];
+  for (const question in questions) {
+    tableList.push(<QuestionTable question={question} answers={questions[question]} />);
   }
-  return tableList;
+  return <>{tableList}</>;
 };
--- src/pages/negotiation/index.tsx
+++ src/pages/negotiation/index.tsx
@@ -1,14 +1,14 @@
 import React from "react";
 import Head from "next/head";
 import { Negotiations } from "../../data";
-import { parseQuestions } from "../../negotiation";
+import { QuestionList } from "../../negotiation";
 
 const NegotiationInfo = () => (
   <>
     <Head>
       <title>Negotiation - rNav</title>
     </Head>
-    <div className="questionlist">{parseQuestions(Negotiations)}</div>
+    <div className="questionlist"><QuestionList questions={Negotiations} /></div>
   </>
 );
 export default NegotiationInfo;
