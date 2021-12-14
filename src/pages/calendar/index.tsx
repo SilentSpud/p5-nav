e5c7bf06e2278a28b037bb55ba35c5a7c86666e3
Silent Spud <1478389+SilentSpud@users.noreply.github.com>
2021-12-14 08:39:10 -0700

    remove localstorage it added ssr inconsistencies

--- src/pages/calendar/index.tsx
+++ src/pages/calendar/index.tsx
@@ -1,10 +1,9 @@
 import React from "react";
 import Head from "next/head";
-import useStorage from "react-use-localstorage2";
 import { Calendar, Day, LoadQuestions, Month, Navbar } from "../../calendar";
 
 const EventCalendar = () => {
-  const [month, setMonth] = useStorage("date", "2016-04-01T06:00:00.000Z"),
+  const [month, setMonth] = React.useState("2016-04-01T06:00:00.000Z"),
     monthHandler = (date: Date) => setMonth(JSON.stringify(date).replace(/"/g, "")),
     classQs = LoadQuestions(),
     monthDate = new Date(month as string);
