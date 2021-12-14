802bd42dfbfe22b5781a4a78815a333e2fa50383
Silent Spud <1478389+SilentSpud@users.noreply.github.com>
2021-12-14 10:52:45 -0700

    add footer for pagination

---
+++ src/table/footer.tsx
@@ -0,0 +1,57 @@
+import React from "react";
+import { Nav } from "react-bootstrap";
+import { FaAngleDoubleLeft, FaAngleDoubleRight, FaAngleLeft, FaAngleRight } from "react-icons/fa";
+import { useTableInfo } from ".";
+
+const NavLink = ({
+  children,
+  disabled,
+  active,
+  onClick,
+}: {
+  children?: React.ReactNode | React.ReactNode[];
+  disabled?: boolean;
+  active?: boolean;
+  onClick?: React.MouseEventHandler<HTMLElement>;
+}) => {
+  return (
+    <Nav.Item>
+      <Nav.Link {...{ disabled, active, onClick }}>{children}</Nav.Link>
+    </Nav.Item>
+  );
+};
+
+export const Footer = () => {
+  const {
+    canPreviousPage,
+    canNextPage,
+    pageOptions,
+    pageCount,
+    gotoPage,
+    nextPage,
+    previousPage,
+    state: { pageIndex },
+  } = useTableInfo();
+  return (
+    <footer>
+      <Nav justify variant="pills">
+        <NavLink onClick={() => gotoPage(0)} disabled={pageIndex > 0}>
+          <FaAngleDoubleLeft />
+        </NavLink>
+        <NavLink onClick={() => previousPage()} disabled={!canPreviousPage}>
+          <FaAngleLeft />
+        </NavLink>
+        <NavLink disabled active>
+          Page {pageIndex + 1} of {pageOptions.length}
+        </NavLink>
+        <NavLink onClick={() => nextPage()} disabled={!canNextPage}>
+          <FaAngleRight />
+        </NavLink>
+        <NavLink onClick={() => gotoPage(pageCount - 1)} disabled={pageIndex < pageCount - 1}>
+          <FaAngleDoubleRight />
+        </NavLink>
+      </Nav>
+    </footer>
+  );
+};
+export default Footer;
--- src/table/index.tsx
+++ src/table/index.tsx
@@ -1,4 +1,5 @@
 export * from "./tableMaker";
 export * from "./header";
+export * from "./footer";
 import PrepareTable from "./tableMaker";
 export default PrepareTable;
