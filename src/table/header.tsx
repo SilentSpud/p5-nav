8671958bd914635f22acebce3c4b0e5b1decaa5e
Silent Spud <1478389+SilentSpud@users.noreply.github.com>
2021-12-14 10:52:32 -0700

    update header to use table context

--- src/table/header.tsx
+++ src/table/header.tsx
@@ -1,45 +1,59 @@
 import { FaSortUp, FaSortDown, FaSort } from "react-icons/fa";
 import React from "react";
 import { CloseButton } from "react-bootstrap";
-import { HeaderGroup } from "react-table";
+import { useTableInfo } from ".";
 
 export const SearchForm = ({ filter }: { filter: (filterValue: string | undefined) => void }) => {
   const searchRef = React.createRef<HTMLInputElement>();
   return React.useMemo(
     () => (
       <div className="search">
-        <input type="search" placeholder="Search" className="text-light w-100 h-100" ref={searchRef} onChange={({ currentTarget: { value } }) => filter(value || undefined)} />
-        <CloseButton className="clear" onClick={() => {
-          filter(undefined);
-          if (!searchRef.current) return;
-          searchRef.current.value = "";
-          searchRef.current.focus();
-        }} />
+        <input
+          type="search"
+          placeholder="Search"
+          className="text-light w-100 h-100"
+          ref={searchRef}
+          onChange={({ currentTarget: { value } }) => filter(value || undefined)}
+        />
+        <CloseButton
+          className="clear"
+          onClick={() => {
+            filter(undefined);
+            if (!searchRef.current) return;
+            searchRef.current.value = "";
+            searchRef.current.focus();
+          }}
+        />
       </div>
     ),
     [filter, searchRef]
   );
-}
+};
 
-export const Header = (headers: HeaderGroup[], setGlobalFilter: (filterValue: string | undefined) => void) => {
-  return headers.map((row, i) => (
-    <div {...row.getHeaderGroupProps()} key={i}>
-      {row.headers.map((cell) => {
-        if (cell.id == "title_0") {
-          return (
-            <div {...cell.getHeaderProps({ className: "searchbox th" })}>
-              <SearchForm filter={setGlobalFilter} />
-            </div>
-          );
-        } else {
-          return (
-            <div {...cell.getHeaderProps({ ...cell.getSortByToggleProps(), className: "th" })}>
-              {cell.render("Header")}
-              {cell.canSort && <span>{(cell.isSortedDesc && <FaSortUp />) || (cell.isSorted && <FaSortDown />) || <FaSort />}</span>}
-            </div>
-          );
-        }
-      })}
-    </div>
-  ));
+export const Header = () => {
+  const { headerGroups, setGlobalFilter } = useTableInfo();
+  return (
+    <header role="rowgroup">
+      {headerGroups.map((row, i) => (
+        <div {...row.getHeaderGroupProps()} key={i}>
+          {row.headers.map((cell) => {
+            if (cell.id == "title_0") {
+              return (
+                <div {...cell.getHeaderProps({ className: "searchbox th" })}>
+                  <SearchForm filter={setGlobalFilter} />
+                </div>
+              );
+            } else {
+              return (
+                <div {...cell.getHeaderProps({ ...cell.getSortByToggleProps(), className: "th" })}>
+                  {cell.render("Header")}
+                  {cell.canSort && <span>{(cell.isSortedDesc && <FaSortUp />) || (cell.isSorted && <FaSortDown />) || <FaSort />}</span>}
+                </div>
+              );
+            }
+          })}
+        </div>
+      ))}
+    </header>
+  );
 };
