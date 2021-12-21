8ddc2021d7ed7296e9a9ce4a046162a351047a84
Silent Spud <1478389+SilentSpud@users.noreply.github.com>
2021-12-21 12:24:20 -0700

    change Questions data to Events

--- src/calendar/EventCalendar.tsx
+++ src/calendar/EventCalendar.tsx
@@ -1,16 +1,15 @@
 import { useMemo } from "react";
 import { parseYear, EventItem } from "./CalendarController";
-import { Questions } from "../data";
-
+import { Events } from "../data";
 export const LoadQuestions = () =>
   useMemo(() => {
     const questions: EventItem[] = [];
-    for (const day of Questions) {
+    for (const day of Events.Questions) {
       const event: EventItem = {
         title: "Classroom Questions",
         date: parseYear(day.date),
         type: "class",
-        questions: day.questions,
+        questions: day.events,
       };
       questions.push(event);
     }
---
+++ src/data/Events.ts
@@ -0,0 +1,21 @@
+import questions from "../../data/questions.json";
+export type EventList<EventProps extends EventDetails> = EventDate<EventProps>[];
+export type EventDate<EventProps> = {
+  date: string;
+  events: EventProps[];
+};
+export type EventDetails = {
+  type: string;
+};
+
+export interface Question extends EventDetails {
+  question: string;
+  answer: string;
+}
+
+export const Events: {
+  Questions: EventList<Question>;
+} = {
+  Questions: questions,
+};
+export default Events;
