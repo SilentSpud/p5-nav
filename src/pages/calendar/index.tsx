ba9974cff2435c1da69bebf6f86800fc95622e68
Silent Spud <1478389+SilentSpud@users.noreply.github.com>
2022-01-03 10:42:37 -0700

    re-implement storing current week in localstorage

--- src/pages/calendar/index.tsx
+++ src/pages/calendar/index.tsx
@@ -1,9 +1,10 @@
 import React from "react";
 import { Week } from "../../calendar";
 import Head from "next/head";
+import useStorage from "react-use-localstorage2";
 
 export const WeekCalendar = () => {
-  const [week, setWeek] = React.useState("2016-04-09T06:00:00.000Z"),
+  const [week, setWeek] = useStorage("date", "2016-04-09T06:00:00.000Z"),
     weekHandler = (date: Date) => setWeek(JSON.stringify(date).replace(/"/g, "")),
     weekDate = new Date(week as string);
   return (
