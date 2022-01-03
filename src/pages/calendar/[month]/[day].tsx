5bbb22849738b3bee86a52d55162ce98ac66dc07
Silent Spud <1478389+SilentSpud@users.noreply.github.com>
2022-01-03 14:28:17 -0700

    add selected date to title

--- src/pages/calendar/[month]/[day].tsx
+++ src/pages/calendar/[month]/[day].tsx
@@ -4,6 +4,7 @@ import { useRouter } from "next/router";
 import { useCookies } from "react-cookie";
 import type { GetServerSideProps } from "next";
 import { Week } from "../../../calendar";
+import { dateToString } from "../../../events";
 
 export const getServerSideProps: GetServerSideProps = async ({ req: { cookies } }) => {
   const week = cookies.week ?? "2016-04-09T06:00:00.000Z";
@@ -24,12 +25,13 @@ export const WeekCalendar = ({ date: inDate }: { date?: string }) => {
   if (month && typeof month !== "string") month = month[0];
   if (day && typeof day !== "string") day = day[0];
   const activeDate = !month ? undefined : new Date(parseInt(month) < 4 ? 2017 : 2016, parseInt(month) - 1, parseInt(day ?? "1"));
+  const dateStr = activeDate ? dateToString(activeDate, true) + " - " : undefined;
 
   return (
     <>
       <Head>
-        <title>Weekly Calendar - rNav</title>
-        <meta property="og:title" content="Weekly Calendar - royal Navigator" />
+        <title>{dateStr ? dateStr : "Weekly"} Calendar - rNav</title>
+        <meta property="og:title" content={`${dateStr ? dateStr + " - " : ""}Event Calendar - royal Navigator`} />
         <meta property="og:description" content="Beta weekly calendar" />
       </Head>
       <Week week={date} setWeek={weekHandler} selected={activeDate} />
