9ba462dde209dba6b810775bce66b809e90f96aa
Silent Spud <1478389+SilentSpud@users.noreply.github.com>
2022-01-03 14:04:31 -0700

    fix link parameters

--- src/calendar/Week.tsx
+++ src/calendar/Week.tsx
@@ -1,5 +1,7 @@
 import React, { useMemo } from "react";
-import { addDays, isSameDay } from "date-fns";
+import Link from "next/link";
+import { useRouter } from "next/router";
+import { addDays, getDate, getMonth, isSameDay } from "date-fns";
 import { Col, Container, Row, Stack } from "react-bootstrap";
 import { WeekNavbar, WeekController, useWeek, WeekDay } from ".";
 import { dateToString, Events, useEvents } from "../events";
@@ -9,8 +11,11 @@ const theWeek = ["Sunday", "Monday", "Tuesday", "Wednesday", "Thursday", "Friday
 const Slot = ({ className, disabled, children }) => <div className={`${className}${disabled ? " invisible" : ""}`}>{!disabled ? children : null}</div>;
 
 const WeekDayBrief = ({ index }: { index: number }) => {
-  const { week, setSelected, selected } = useWeek();
+  const { week, selected, setSelected } = useWeek();
   const { getEvents } = useEvents();
+  const {
+    query: { month, day },
+  } = useRouter();
   return useMemo(() => {
     const newDate = addDays(week, index);
     const isSelected = selected ? isSameDay(newDate, selected) : false;
@@ -26,22 +31,26 @@ const WeekDayBrief = ({ index }: { index: number }) => {
         </Stack>
       </Col>
     ) : (
-      <Col onClick={() => setSelected(newDate)} className={`week-day${isSelected ? " selected" : ""}`}>
-        <Stack gap={0}>
-          <div className="weekday-title">
-            <span className="left">{theWeek[index]}</span>
-            <span className="right">{dateToString(newDate, false)}</span>
-          </div>
-          <Slot className="day noon" disabled={!afternoon?.events?.length}>
-            Daytime Events
-          </Slot>
-          <Slot className="day night" disabled={!evening?.events?.length && !night?.events?.length}>
-            Evening Events
-          </Slot>
-        </Stack>
+      <Col className={`week-day${isSelected ? " selected" : ""}`}>
+        <Link href={`/calendar/${getMonth(newDate) + 1}/${getDate(newDate)}`} replace={!!month && !!day} passHref shallow>
+          <a onClick={() => setSelected(newDate)}>
+            <Stack gap={0}>
+              <div className="weekday-title">
+                <span className="left">{theWeek[index]}</span>
+                <span className="right">{dateToString(newDate, false)}</span>
+              </div>
+              <Slot className="day noon" disabled={!afternoon?.events?.length}>
+                Daytime Events
+              </Slot>
+              <Slot className="day night" disabled={!evening?.events?.length && !night?.events?.length}>
+                Evening Events
+              </Slot>
+            </Stack>
+          </a>
+        </Link>
       </Col>
     );
-  }, [getEvents, index, selected, setSelected, week]);
+  }, [getEvents, index, day, month, selected, setSelected, week]);
 };
 
 export const Week = ({ week, setWeek, selected }: { week: Date; setWeek: (date: Date) => void; selected?: Date }) =>
