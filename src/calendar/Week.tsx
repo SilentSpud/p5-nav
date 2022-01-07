95e0f846d7e33cb0e1635fed3a4481c724eea32b
Silent Spud <1478389+SilentSpud@users.noreply.github.com>
2022-01-07 01:56:07 -0700

    add free time to week display

--- src/calendar/Week.tsx
+++ src/calendar/Week.tsx
@@ -4,13 +4,34 @@ import { useRouter } from "next/router";
 import { addDays, getDate, getMonth, isSameDay } from "date-fns";
 import { Col, Container, Row, Stack } from "react-bootstrap";
 import { WeekNavbar, WeekController, useWeek, WeekDay } from ".";
-import { dateToString, Events, useEvents } from "../events";
+import { dateToString, Events, FreeTime, useEvents } from "../events";
 
 const theWeek = ["Sunday", "Monday", "Tuesday", "Wednesday", "Thursday", "Friday", "Saturday"];
 
 const Slot: FC<{ className: string; disabled?: boolean }> = ({ className, disabled, children }) => (
-  <div className={`${className}${disabled ? " invisible" : ""}`}>{!disabled ? children : null}</div>
+  <div className={`day ${className} ${disabled ? "invisible" : ""}`}>{!disabled ? children : null}</div>
 );
+const TimeSlot: FC<{ slot?: FreeTime; night?: boolean }> = ({ slot, night }) => {
+  let className: string = "",
+    text: string = "";
+  switch (slot) {
+    case FreeTime.None: {
+      className = "invisible";
+      break;
+    }
+    case FreeTime.Leblanc: {
+      className = "border-secondary";
+      text = (night ? "Night" : "Evening") + ": Leblanc Only";
+      break;
+    }
+    case FreeTime.Full: {
+      className = "border-primary";
+      text = (night ? "Night" : "Evening") + ": Free Time";
+      break;
+    }
+  }
+  return <div className={`day border ${className}`}>{text}</div>;
+};
 
 const BadDay: FC<{ index: number; date: Date }> = ({ index, date }) => (
   <Col className="week-day empty">
@@ -33,11 +54,11 @@ const WeekDayBrief: FC<{ index: number }> = ({ index }) => {
     const newDate = addDays(week, index);
     const isSelected = selected ? isSameDay(newDate, selected) : false;
     const { afternoon, evening, night } = getEvents(newDate);
-    const disabled = !afternoon && !evening && !night;
-    return disabled ? (
+
+    return !afternoon && !evening && !night ? (
       <BadDay index={index} date={newDate} />
     ) : (
-      <Col className={`week-day${isSelected ? " selected" : ""}`}>
+      <Col className={`week-day ${isSelected ? "selected" : ""}`}>
         <Link href={`/calendar/${getMonth(newDate) + 1}/${getDate(newDate)}`} replace={!!month && !!day} passHref shallow>
           <a onClick={() => setSelected(newDate)} className="week-link">
             <Stack gap={0}>
@@ -45,12 +66,14 @@ const WeekDayBrief: FC<{ index: number }> = ({ index }) => {
                 <span className="left">{theWeek[index]}</span>
                 <span className="right">{dateToString(newDate, false)}</span>
               </div>
-              <Slot className="day noon" disabled={!afternoon?.events?.length}>
+              <Slot className="noon" disabled={!afternoon?.events?.length}>
                 Daytime Events
               </Slot>
-              <Slot className="day night" disabled={!evening?.events?.length && !night?.events?.length}>
+              <Slot className="night" disabled={!evening?.events?.length && !night?.events?.length}>
                 Evening Events
               </Slot>
+              <TimeSlot slot={evening?.time} />
+              <TimeSlot slot={night?.time} night />
             </Stack>
           </a>
         </Link>
