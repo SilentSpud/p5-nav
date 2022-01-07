2d20d8204be648c302699b5d83a19ba4bf2dd3b7
Silent Spud <1478389+SilentSpud@users.noreply.github.com>
2022-01-07 00:14:20 -0700

    Set week based on URL

--- src/pages/calendar/[month]/[day].tsx
+++ src/pages/calendar/[month]/[day].tsx
@@ -1,4 +1,4 @@
-import React, { useState } from "react";
+import React, { useState, FC } from "react";
 import Head from "next/head";
 import { useRouter } from "next/router";
 import { useCookies } from "react-cookie";
@@ -11,15 +11,7 @@ export const getServerSideProps: GetServerSideProps = async ({ req: { cookies } 
   return { props: { date: week } };
 };
 
-export const WeekCalendar = ({ date: inDate }: { date?: string }) => {
-  const [cookies, setCookie] = useCookies(["week"]);
-  const week = inDate ?? cookies.week ?? "2016-04-09T06:00:00.000Z";
-  const [date, setDate] = useState<Date>(new Date(week));
-  const weekHandler = (date: Date) => {
-    setDate(date);
-    setCookie("week", JSON.stringify(date).replace(/"/g, ""));
-  };
-
+export const WeekCalendar: FC<{ date?: string }> = ({ date: inDate }) => {
   const router = useRouter();
   let { month, day } = router.query;
   if (month && typeof month !== "string") month = month[0];
@@ -27,10 +19,18 @@ export const WeekCalendar = ({ date: inDate }: { date?: string }) => {
   const activeDate = !month ? undefined : new Date(parseInt(month) < 4 ? 2017 : 2016, parseInt(month) - 1, parseInt(day ?? "1"));
   const dateStr = activeDate ? dateToString(activeDate, true) + " - " : undefined;
 
+  const [cookies, setCookie] = useCookies(["week"]);
+  const week = activeDate ?? inDate ?? cookies.week ?? "2016-04-09T06:00:00.000Z";
+  const [date, setDate] = useState<Date>(new Date(week));
+  const weekHandler = (date: Date) => {
+    setDate(date);
+    setCookie("week", JSON.stringify(date).replace(/"/g, ""));
+  };
+
   return (
     <>
       <Head>
-        <title>{dateStr ? dateStr : "Weekly"} Calendar - rNav</title>
+        <title>{dateStr ?? "Weekly"} Calendar - rNav</title>
         <meta property="og:title" content={`${dateStr ? dateStr + " - " : ""}Event Calendar - royal Navigator`} />
         <meta property="og:description" content="Beta weekly calendar" />
       </Head>
